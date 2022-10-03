559aeecb64f8a73009f55f1ee554808f007e6b9c
michail <urafeneks@gmail.com>
2022-10-03 18:19:45 +0300

    jiraf

---
+++ exam1.py
@@ -0,0 +1,21 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+# Пример:
+
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+from random import randint
+
+def get_list(n, frst, last):
+    return [randint(frst, last) for i in range(n)]
+
+def sum_odd_position(mylist):
+    return sum(mylist[0::2])
+
+n = 10
+frst = 1
+last = 10
+
+mylist = get_list(n, frst, last)
+print(mylist)
+print(sum_odd_position(mylist))
